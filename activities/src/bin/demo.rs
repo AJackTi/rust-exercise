fca803814210c51d7dc2a9385a55dc01e1eb7af5
trong.dang <trong.dang@vieon.vn>
2023-11-01 15:47:50 +0700

    feat: update loop demo.rs

--- activities/src/bin/demo.rs
+++ activities/src/bin/demo.rs
@@ -1,8 +1,20 @@
+// fn main() {
+//     let age = 15;
+//     if age >= 21 {
+//         println!("valid age")
+//     } else {
+//         println!("invalid age")
+//     }
+// }
+
 fn main() {
-    let age = 15;
-    if age >= 21 {
-        println!("valid age")
-    } else {
-        println!("invalid age")
+    let mut i = 3;
+    loop {
+        if i == 0 {
+            break;
+        }
+        println!("{:?}", i);
+        i-=1;
     }
+    println!("done");
 }

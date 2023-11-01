97b114e038623ae3c446759150103f5c8aeb8c5d
trong.dang <trong.dang@vieon.vn>
2023-11-01 15:02:08 +0700

    feat: update a3a.rs

--- activities/src/bin/a3a.rs
+++ activities/src/bin/a3a.rs
@@ -10,4 +10,14 @@
 // * Use an if..else block to determine which message to display
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+fn main() {
+    // * Displays a message based on the value of a boolean variable
+    let my_bool = true;
+    // * When the variable is set to true, display "hello"
+    // * When the variable is set to false, display "goodbye"
+    if my_bool {
+        println!("hello");
+    } else {
+        println!("goodbye");
+    }
+}

605dda68ac6eb5d6e94f1e74e07356c2ea7f575b
trong.dang <trong.dang@vieon.vn>
2023-11-01 16:02:25 +0700

    feat: update while loop to demo.rs

--- activities/src/bin/demo.rs
+++ activities/src/bin/demo.rs
@@ -7,14 +7,22 @@
 //     }
 // }
 
+// fn main() {
+//     let mut i = 3;
+//     loop {
+//         if i == 0 {
+//             break;
+//         }
+//         println!("{:?}", i);
+//         i-=1;
+//     }
+//     println!("done");
+// }
+
 fn main() {
-    let mut i = 3;
-    loop {
-        if i == 0 {
-            break;
-        }
+    let mut i = 1;
+    while i <=3 {
         println!("{:?}", i);
-        i-=1;
+        i += 1;
     }
-    println!("done");
 }

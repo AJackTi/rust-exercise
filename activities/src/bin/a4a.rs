06f5ea79183e9fdc5ecbf2f474a9b7bfa6d8adc8
trong.dang <trong.dang@vieon.vn>
2023-11-01 15:32:26 +0700

    feat: update a4a.rs

--- activities/src/bin/a4a.rs
+++ activities/src/bin/a4a.rs
@@ -7,4 +7,14 @@
 // * Use a variable set to either true or false
 // * Use a match expression to determine which message to display
 
-fn main() {}
+fn main() {
+    // * Display "it's true" or "it's false" based on the value of a variable
+    // * Use a variable set to either true or false
+    let my_bool = false;
+
+    // * Use a match expression to determine which message to display
+    match my_bool {
+        true => println!("it's true"),
+        false => println!("it's false")
+    }
+}

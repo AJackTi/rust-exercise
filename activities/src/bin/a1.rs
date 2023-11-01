8f6553f5c8df84b93f49fdbe7edead0611ed06b4
trong.dang <trong.dang@vieon.vn>
2023-11-01 14:38:22 +0700

    feat: update a1.rs

--- activities/src/bin/a1.rs
+++ activities/src/bin/a1.rs
@@ -8,4 +8,17 @@
 // * Use a function to display your last name
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+// * Use a function to display your first name
+fn first_name() {
+    println!("Ti")
+}
+
+// * Use a function to display your last name
+fn last_name() {
+    println!("AJack")
+}
+
+fn main() {
+    first_name();
+    last_name();
+}

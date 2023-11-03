b1527fd82f19e42774694c67790e198b7a86881e
trong.dang <dtrong97vn@gmail.com>
2023-11-03 11:34:38 +0700

    feat: impl struct, Result and match a18.rs

--- activities/src/bin/a18.rs
+++ activities/src/bin/a18.rs
@@ -16,4 +16,33 @@
 //   * For the Ok variant, print any message you want
 //   * For the Err variant, print out the error message
 
-fn main() {}
+#[derive(Debug)]
+struct Adult {
+    age: u8,
+    name: String,
+}
+
+impl Adult {
+    fn new(age: u8, name: &str) -> Result<Self, &str> {
+        if age >= 21 {
+            Ok(Self { age, name: name.to_string() })
+        } else {
+            Err("Age must be at least 21")
+        }
+    }
+}
+
+fn main() {
+    let child = Adult::new(15, "Anita");
+    let adult = Adult::new(21, "Sanjay");
+
+    match child {
+        Ok(child) => println!("{} is {} years old", child.name, child.age),
+        Err(e) => println!("{e}"),
+    }
+
+    match adult {
+        Ok(adult) => println!("{} is {} years old", adult.name, adult.age),
+        Err(e) => println!("{e}"),
+    }
+}

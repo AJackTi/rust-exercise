4a2f81d9f6b349a89641d51d96c207abf33aebd4
trong.dang <dtrong97vn@gmail.com>
2023-11-03 13:01:28 +0700

    feat: hashmap a19.rs

--- activities/src/bin/a19.rs
+++ activities/src/bin/a19.rs
@@ -13,4 +13,21 @@
 // Notes:
 // * Use a HashMap for the furniture store stock
 
-fn main() {}
+use std::collections::HashMap;
+
+fn main() {
+    let mut stock = HashMap::new();
+    stock.insert("Chair", 5);
+    stock.insert("Bed", 3);
+    stock.insert("Table", 2);
+    stock.insert("Couch", 0);
+
+    let mut total_stock = 0;
+    for (item, qty) in stock.iter() {
+        let stock_count = if qty == &0 { "out of stock".to_owned() } else { format!("{:?}", qty) };
+        println!("item: {}, stock: {}", item, stock_count);
+        total_stock += qty;
+    }
+
+    println!("total stock: {:?}", total_stock);
+}

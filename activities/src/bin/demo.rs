df380fd37f07a45988919db4b1eda3d69937cfdb
trong.dang <dtrong97vn@gmail.com>
2023-11-03 09:37:18 +0700

    feat: cargo doc demo.rs

--- activities/src/bin/demo.rs
+++ activities/src/bin/demo.rs
@@ -265,31 +265,49 @@
 //     }
 // }
 
-struct Survey {
-    q1: Option<i32>,
-    q2: Option<bool>,
-    q3: Option<String>,
+// struct Survey {
+//     q1: Option<i32>,
+//     q2: Option<bool>,
+//     q3: Option<String>,
+// }
+
+// fn main() {
+//     let response = Survey {
+//         q1: None,
+//         q2: Some(true),
+//         q3: Some("A".to_owned()),
+//     };
+
+//     match response.q1 {
+//         Some(ans) => println!("q1: {:?}", ans),
+//         None => println!("q1: no response"),
+//     }
+
+//     match response.q2 {
+//         Some(ans) => println!("q2: {:?}", ans),
+//         None => println!("q2: no response"),
+//     }
+
+//     match response.q3 {
+//         Some(ans) => println!("q3: {:?}", ans),
+//         None => println!("q3: no response"),
+//     }
+// }
+
+enum Color {
+    Red,
+    Blue,
 }
 
-fn main() {
-    let response = Survey {
-        q1: None,
-        q2: Some(true),
-        q3: Some("A".to_owned()),
-    };
-
-    match response.q1 {
-        Some(ans) => println!("q1: {:?}", ans),
-        None => println!("q1: no response"),
-    }
-
-    match response.q2 {
-        Some(ans) => println!("q2: {:?}", ans),
-        None => println!("q2: no response"),
-    }
-
-    match response.q3 {
-        Some(ans) => println!("q3: {:?}", ans),
-        None => println!("q3: no response"),
-    }
+/// A piece of mail
+struct Mail {
+    /// The destination address.
+    address: String,
 }
+
+/// Adds two numbers together.
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+fn main() {}

8d3e34ee76ab66f352462cd0d842f0ab7bd280c0
trong.dang <dtrong97vn@gmail.com>
2023-11-04 16:35:58 +0700

    feat: chrono package a26.rs

--- activities/src/bin/a26.rs
+++ activities/src/bin/a26.rs
@@ -8,4 +8,11 @@
 // * (OPTIONAL) Read the documentation section `Formatting and Parsing`
 //   for examples on how to create custom time formats
 
-fn main() {}
+use chrono::prelude::*;
+
+fn main() {
+    let utc: DateTime<Utc> = Utc::now();
+    dbg!(utc);
+    let local: DateTime<Local> = Local::now();
+    dbg!(local);
+}

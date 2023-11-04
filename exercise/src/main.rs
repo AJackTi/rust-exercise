ff28c863c8f178e2bc74c70f58dbe2b606c86ba2
trong.dang <dtrong97vn@gmail.com>
2023-11-04 22:06:16 +0700

    feat: function unique exercise

--- exercise/src/main.rs
+++ exercise/src/main.rs
@@ -14,8 +14,37 @@ fn median(mut a: Vec<f32>) -> Option<f32> {
     Some(med)
 }
 
-fn main() {
-    let answer = median(vec![1.0, 2.0, 5.0, 6.0]);
+// fn unique(mut a: Vec<i32>) -> Vec<i32> {
+//     // let mut tmp = Vec::new();
 
-    dbg!(answer);
+//     // for elem in a {
+//     //     if tmp.contains(&elem) {
+//     //         continue;
+//     //     }
+//     //     tmp.push(elem);
+//     // }
+//     // tmp
+// }
+
+// fn unique(mut a: Vec<i32>) -> Vec<i32> {
+//     a.sort_unstable();
+//     a.dedup();
+//     a
+// }
+
+// fn unique<T: Ord>(mut a: Vec<T>) -> Vec<T> {
+//     a.sort_by(|x: &T, y: &T| x.cmp(y));
+//     a.dedup();
+//     a
+// }
+
+fn main() {
+    // let answer = median(vec![1.0, 2.0, 5.0, 6.0]);
+    // dbg!(answer);
+
+    // let answer = unique(vec![1, 1, 2, 3, 4, 4, 5]);
+    // dbg!(answer);
+
+    // let answer = unique(vec![1, 2, 3, 4, 4, 5, 5]);
+    // dbg!(answer);
 }

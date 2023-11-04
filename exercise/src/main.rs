3c8e7c74fb168219c16ad7ced2c40abe740d69b0
trong.dang <dtrong97vn@gmail.com>
2023-11-04 22:57:51 +0700

    feat: print morse code

--- exercise/src/main.rs
+++ exercise/src/main.rs
@@ -38,10 +38,64 @@
 //     a
 // }
 
-fn sort_username<T: AsRef<str>>(username: &mut Vec<T>) {
-    username.sort_by_cached_key(|x| { x.as_ref().to_lowercase() });
+// fn sort_username<T: AsRef<str>>(username: &mut Vec<T>) {
+//     username.sort_by_cached_key(|x| { x.as_ref().to_lowercase() });
 
-    // username.sort_by(|x, y| x.as_ref().to_lowercase().cmp(&y.as_ref().to_lowercase()))
+//     // username.sort_by(|x, y| x.as_ref().to_lowercase().cmp(&y.as_ref().to_lowercase()))
+// }
+
+#[derive(Debug, PartialEq)]
+enum Pulse {
+    Short,
+    Long,
+}
+
+// Represents a single character
+type Letter = Vec<Pulse>;
+
+// Represents a string of characters
+type Message = Vec<Letter>;
+
+trait MorseCode {
+    fn to_morse_code(&self) -> Message;
+}
+
+impl MorseCode for String {
+    fn to_morse_code(&self) -> Message {
+        use Pulse::*;
+
+        let mut msg = Vec::with_capacity(self.len());
+        for c in self.chars() {
+            let morse = match c {
+                'A' | 'a' => vec![Short, Long],
+                _ => {
+                    continue;
+                }
+            };
+            msg.push(morse);
+        }
+
+        msg
+    }
+}
+
+impl std::fmt::Display for Pulse {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Pulse::Short => write!(f, "."),
+            Pulse::Long => write!(f, "_"),
+        }
+    }
+}
+
+fn print_morse_code(code: &Message) {
+    for letter in code.iter() {
+        for pulse in letter.iter() {
+            print!("{}", pulse);
+        }
+        print!(" ");
+    }
+    println!();
 }
 
 fn main() {
@@ -54,7 +108,10 @@ fn main() {
     // let answer = unique(vec![1, 2, 3, 4, 4, 5, 5]);
     // dbg!(answer);
 
-    let mut users = vec!["Todd", "Amy", "Jennifer", "Tommy"];
-    sort_username(&mut users);
-    dbg!(users);
+    // let mut users = vec!["Todd", "Amy", "Jennifer", "Tommy"];
+    // sort_username(&mut users);
+    // dbg!(users);
+
+    let greeting = "aaa hello, world".to_string().to_morse_code();
+    print_morse_code(&greeting)
 }

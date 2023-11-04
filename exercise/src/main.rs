7a3a6d861d82c3bfb2b0e019156a6977dc01b774
trong.dang <dtrong97vn@gmail.com>
2023-11-04 21:47:11 +0700

    feat: mean function exercise

--- exercise/src/main.rs
+++ exercise/src/main.rs
@@ -1,3 +1,21 @@
+fn median(mut a: Vec<f32>) -> Option<f32> {
+    if a.is_empty() {
+        return None;
+    }
+
+    a.sort_by(|x, y| { x.partial_cmp(y).unwrap() });
+
+    let n_elements = a.len();
+    let middle = n_elements / 2;
+    let a_is_even = n_elements % 2 == 0;
+
+    let med = if a_is_even { (a[middle] + a[middle - 1]) / 2.0 } else { a[middle] };
+
+    Some(med)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let answer = median(vec![1.0, 2.0, 5.0, 6.0]);
+
+    dbg!(answer);
 }

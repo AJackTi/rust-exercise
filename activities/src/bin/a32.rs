effc72a2bb86d5a54dbd1369e44238053d09c253
trong.dang <dtrong97vn@gmail.com>
2023-11-07 20:40:21 +0700

    feat: lifetime struct a32.rs

--- activities/src/bin/a32.rs
+++ activities/src/bin/a32.rs
@@ -12,4 +12,40 @@
 
 const MOCK_DATA: &'static str = include_str!("mock-data.csv");
 
-fn main() {}
+struct Names<'a> {
+    inner: Vec<&'a str>,
+}
+
+struct Titles<'a> {
+    inner: Vec<&'a str>,
+}
+
+fn main() {
+    let data: Vec<_> = MOCK_DATA.split('\n').skip(1).collect();
+    let names: Vec<_> = data
+        .iter()
+        .filter_map(|line| line.split(',').nth(1))
+        .collect();
+    let names = Names { inner: names };
+
+    let titles: Vec<_> = data
+        .iter()
+        .filter_map(|line| line.split(',').nth(4))
+        .collect();
+
+    let titles = Titles { inner: titles };
+
+    // for n in names.inner.iter().take(3) {
+    //     println!("{}", n);
+    // }
+
+    // for n in titles.inner.iter().take(3) {
+    //     println!("{}", n);
+    // }
+
+    let data = names.inner.iter().zip(titles.inner.iter());
+
+    for (name, title) in data {
+        println!("Name: {}, title: {}", name, title);
+    }
+}

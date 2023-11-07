3d22b6be80d85cb1ffdbd4558b04bd6a33930fe4
trong.dang <dtrong97vn@gmail.com>
2023-11-07 21:30:01 +0700

    feat: demo custom error

---
+++ demos/demo-custom-error/src/bin/1.rs
@@ -0,0 +1,51 @@
+use chrono::{ DateTime, Duration, Utc };
+use thiserror::Error;
+
+struct SubwayPass {
+    id: usize,
+    funds: isize,
+    expires: DateTime<Utc>,
+}
+
+#[derive(Debug, Error)]
+enum PassError {
+    #[error("expired pass")]
+    PassExpired,
+    #[error("insufficient funds: {0}")] InsufficientFunds(isize),
+    #[error("pass read error: {0}")] ReadError(String),
+}
+
+fn swipe_card() -> Result<SubwayPass, PassError> {
+    // Err(PassError::ReadError("Magstrip failed to read".to_owned()))
+    Ok(SubwayPass {
+        id: 0,
+        funds: 200,
+        expires: Utc::now() + Duration::weeks(52),
+    })
+}
+
+fn use_pass(pass: &mut SubwayPass, cost: isize) -> Result<(), PassError> {
+    if Utc::now() > pass.expires {
+        Err(PassError::PassExpired)
+    } else {
+        if pass.funds - cost < 0 {
+            Err(PassError::InsufficientFunds(pass.funds))
+        } else {
+            pass.funds = pass.funds - cost;
+            Ok(())
+        }
+    }
+}
+
+fn main() {
+    let pass_status = swipe_card().and_then(|mut pass| use_pass(&mut pass, 3));
+    match pass_status {
+        Ok(_) => println!("ok to board"),
+        Err(e) =>
+            match e {
+                PassError::ReadError(e) => (),
+                PassError::PassExpired => (),
+                PassError::InsufficientFunds(f) => (),
+            }
+    }
+}

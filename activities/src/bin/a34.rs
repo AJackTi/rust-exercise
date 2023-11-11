975162fc8d0df9da9d4f1c4d2b300701cad1757f
trong.dang <dtrong97vn@gmail.com>
2023-11-11 16:51:54 +0700

    feat: TypeState pattern a34.rs

--- activities/src/bin/a34.rs
+++ activities/src/bin/a34.rs
@@ -16,4 +16,52 @@
 // Notes:
 // * Optionally use generics for each state
 
-fn main() {}
+#[derive(Clone, Copy)]
+struct LuggageId(usize);
+struct Luggage(LuggageId);
+struct CheckIn(LuggageId);
+struct OnLoad(LuggageId);
+struct OffLoad(LuggageId);
+struct AwaitingPickup(LuggageId);
+struct EndCustody(LuggageId);
+
+impl Luggage {
+    fn new(id: LuggageId) -> Self {
+        Luggage(id)
+    }
+
+    fn check_in(self) -> CheckIn {
+        CheckIn(self.0)
+    }
+}
+
+impl CheckIn {
+    fn onload(self) -> OnLoad {
+        OnLoad(self.0)
+    }
+}
+
+impl OnLoad {
+    fn offload(self) -> OffLoad {
+        OffLoad(self.0)
+    }
+}
+
+impl OffLoad {
+    fn carousel(self) -> AwaitingPickup {
+        AwaitingPickup(self.0)
+    }
+}
+
+impl AwaitingPickup {
+    fn pickup(self) -> (Luggage, EndCustody) {
+        (Luggage(self.0), EndCustody(self.0))
+    }
+}
+
+fn main() {
+    let id = LuggageId(1);
+    let luggage = Luggage::new(id);
+    let luggae = luggage.check_in().onload().offload().carousel();
+    let (luggage, _) = luggae.pickup();
+}

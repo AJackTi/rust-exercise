98f9766163fe1f42f32518aeb61966cb075b32c6
trong.dang <dtrong97vn@gmail.com>
2023-11-11 16:06:46 +0700

    feat: TypeState pattern

---
+++ demos/demo-typestate/src/main.rs
@@ -0,0 +1,63 @@
+struct Employee<State> {
+    name: String,
+    state: State,
+}
+
+impl<State> Employee<State> {
+    fn transition<NextState>(self, state: NextState) -> Employee<NextState> {
+        Employee {
+            name: self.name,
+            state: state,
+        }
+    }
+}
+
+struct Agreement;
+struct Signature;
+struct Training;
+struct FailedTraining {
+    score: u8,
+}
+
+struct OnboardingComplete {
+    score: u8,
+}
+
+impl Employee<Agreement> {
+    fn new(name: &str) -> Self {
+        Self {
+            name: name.to_string(),
+            state: Agreement,
+        }
+    }
+
+    fn read_agreement(self) -> Employee<Signature> {
+        self.transition(Signature)
+    }
+}
+
+impl Employee<Signature> {
+    fn sign(self) -> Employee<Training> {
+        self.transition(Training)
+    }
+}
+
+#[rustfmt::skip]
+impl Employee<Training> {
+    fn train(self, score: u8) -> Result<Employee<OnboardingComplete>, Employee<FailedTraining>> {
+        if score >= 7 {
+            Ok(self.transition(OnboardingComplete { score }))
+        } else {
+            Err(self.transition(FailedTraining { score }))
+        }
+    }
+}
+
+fn main() {
+    let employee: Employee<Agreement> = Employee::new("Sara");
+    let onboarded = employee.read_agreement().sign().train(6);
+    match onboarded {
+        Ok(emp) => println!("onboarding complete"),
+        Err(emp) => println!("training failed, score: {}", emp.state.score),
+    }
+}

9c6ac0cd6ae066b5503db413cbfe695e095aa84c
trong.dang <dtrong97vn@gmail.com>
2023-11-13 20:17:58 +0700

    feat: crate

--- activities/src/bin/demo.rs
+++ activities/src/bin/demo.rs
@@ -938,9 +938,36 @@
 //     println!("s = {}", s);
 // }
 
+// fn main() {
+//     let a = String::from("Hello World");
+//     let r1 = &a[0..5];
+//     println!("{}", r1);
+//     println!("{}", a);
+// }
+
+extern crate rand;
+
+use rand::Rng;
+use std::io;
+
 fn main() {
-    let a = String::from("Hello World");
-    let r1 = &a[0..5];
-    println!("{}", r1);
-    println!("{}", a);
+    println!("Guess a number");
+    let secret = rand::thread_rng().gen_range(1, 10);
+    loop {
+        println!("Input your guess");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Fail");
+        let guess: i32 = guess.trim().parse().expect("Fail");
+        if guess == secret {
+            println!("Guessed correctly");
+            break;
+        } else {
+            println!("Try again");
+            if guess > secret {
+                println!("You have guessed a higher no");
+            } else {
+                println!("Value is smaller");
+            }
+        }
+    }
 }

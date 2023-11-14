de4d1ff3788d21668dfeaf0303c9e9cc397f4989
trong.dang <dtrong97vn@gmail.com>
2023-11-14 19:29:30 +0700

    feat: mod

--- activities/src/bin/demo.rs
+++ activities/src/bin/demo.rs
@@ -945,29 +945,41 @@
 //     println!("{}", a);
 // }
 
-extern crate rand;
+// extern crate rand;
 
-use rand::Rng;
-use std::io;
+// use rand::Rng;
+// use std::io;
+
+// fn main() {
+//     println!("Guess a number");
+//     let secret = rand::thread_rng().gen_range(1, 10);
+//     loop {
+//         println!("Input your guess");
+//         let mut guess = String::new();
+//         io::stdin().read_line(&mut guess).expect("Fail");
+//         let guess: i32 = guess.trim().parse().expect("Fail");
+//         if guess == secret {
+//             println!("Guessed correctly");
+//             break;
+//         } else {
+//             println!("Try again");
+//             if guess > secret {
+//                 println!("You have guessed a higher no");
+//             } else {
+//                 println!("Value is smaller");
+//             }
+//         }
+//     }
+// }
+
+mod network {
+    fn connect() {}
+
+    pub mod server {
+        pub fn connect() {}
+    }
+}
 
 fn main() {
-    println!("Guess a number");
-    let secret = rand::thread_rng().gen_range(1, 10);
-    loop {
-        println!("Input your guess");
-        let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("Fail");
-        let guess: i32 = guess.trim().parse().expect("Fail");
-        if guess == secret {
-            println!("Guessed correctly");
-            break;
-        } else {
-            println!("Try again");
-            if guess > secret {
-                println!("You have guessed a higher no");
-            } else {
-                println!("Value is smaller");
-            }
-        }
-    }
+    network::server::connect()
 }

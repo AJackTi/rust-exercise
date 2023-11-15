b11c99166db1850c836bf2c5edf1cd8ed2c1e59b
trong.dang <dtrong97vn@gmail.com>
2023-11-15 20:51:57 +0700

    feat: using ? match Result

--- activities/src/bin/demo.rs
+++ activities/src/bin/demo.rs
@@ -996,24 +996,44 @@
 //     println!("{}", n);
 // }
 
+// use std::io;
+// use std::io::Read;
+// use std::fs::File;
+
+// fn read() -> Result<String, io::Error> {
+//     let f = File::open("hello.txt");
+//     let mut f = match f {
+//         Ok(file) => file,
+//         Err(e) => {
+//             return Err(e);
+//         }
+//     };
+
+//     let mut s = String::new();
+//     match f.read_to_string(&mut s) {
+//         Ok(_) => Ok(s),
+//         Err(e) => Err(e),
+//     }
+// }
+
+// fn main() {
+//     let output = read();
+//     match output {
+//         Ok(fi) => println!("{}", fi),
+//         Err(e) => println!("{}", e),
+//     }
+// }
+
 use std::io;
 use std::io::Read;
 use std::fs::File;
 
 fn read() -> Result<String, io::Error> {
-    let f = File::open("hello.txt");
-    let mut f = match f {
-        Ok(file) => file,
-        Err(e) => {
-            return Err(e);
-        }
-    };
+    let mut f = File::open("hello.txt")?;
 
     let mut s = String::new();
-    match f.read_to_string(&mut s) {
-        Ok(_) => Ok(s),
-        Err(e) => Err(e),
-    }
+    f.read_to_string(&mut s)?;
+    Ok(s)
 }
 
 fn main() {

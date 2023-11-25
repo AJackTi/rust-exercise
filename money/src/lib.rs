d5155e10f07768e7644b787eaa824b3a6d34607a
trong.dang <dtrong97vn@gmail.com>
2023-11-25 09:29:47 +0700

    feat: define own trait

---
+++ money/src/lib.rs
@@ -0,0 +1,54 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[derive(Debug, PartialEq)]
+pub struct USD(i32);
+#[derive(Debug, PartialEq)]
+pub struct GBP(i32);
+#[derive(Debug, PartialEq)]
+pub struct CAD(i32);
+
+pub trait ToUSD {
+    fn to_usd(&self) -> USD;
+    fn convert<T: FromUSD>(&self) -> T {
+        T::from_usd(&self.to_usd())
+    }
+}
+
+pub trait FromUSD {
+    fn from_usd(u: &USD) -> Self;
+}
+
+impl ToUSD for GBP {
+    fn to_usd(&self) -> USD {
+        USD((self.0 * 130) / 100)
+    }
+}
+
+impl FromUSD for CAD {
+    fn from_usd(u: &USD) -> Self {
+        CAD((u.0 * 130) / 100)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+
+        let g = GBP(200);
+        let u = g.to_usd();
+        assert_eq!(u, USD(260));
+
+        let c = CAD::from_usd(&u);
+        assert_eq!(c, CAD(338));
+
+        let c2: CAD = g.convert();
+        assert_eq!(c2, c);
+    }
+}

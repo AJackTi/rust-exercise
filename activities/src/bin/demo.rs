442a2ffb4d909f3b68fb514e10759ae2b0ce85bf
trong.dang <dtrong97vn@gmail.com>
2023-11-28 19:31:01 +0700

    feat: mutex

--- activities/src/bin/demo.rs
+++ activities/src/bin/demo.rs
@@ -1481,33 +1481,55 @@
 //     println!("Main thread");
 // }
 
-use std::{ sync::mpsc, thread, time::Duration };
+// use std::{ sync::mpsc, thread, time::Duration };
 
-const NUM_THREADS: usize = 20;
+// const NUM_THREADS: usize = 20;
 
-fn start_thread(d: usize, tx: mpsc::Sender<usize>) {
-    thread::spawn(move || {
-        println!("setting timer {}", d);
-        thread::sleep(Duration::from_secs(d as u64));
-        println!("sending {}", d);
-        tx.send(d).unwrap()
-    });
-}
+// fn start_thread(d: usize, tx: mpsc::Sender<usize>) {
+//     thread::spawn(move || {
+//         println!("setting timer {}", d);
+//         thread::sleep(Duration::from_secs(d as u64));
+//         println!("sending {}", d);
+//         tx.send(d).unwrap()
+//     });
+// }
+
+// fn main() {
+//     // let (tx, rx) = mpsc::channel();
+//     // thread::spawn(move || {
+//     //     tx.send(42).unwrap();
+//     // });
+
+//     // println!("received {}", rx.recv().unwrap());
+
+//     let (tx, rx) = mpsc::channel();
+//     for i in 0..NUM_THREADS {
+//         start_thread(i, tx.clone());
+//     }
+
+//     for j in rx.iter().take(NUM_THREADS) {
+//         println!("received {}", j);
+//     }
+// }
+
+use std::{ sync::{ Arc, Mutex }, thread };
 
 fn main() {
-    // let (tx, rx) = mpsc::channel();
-    // thread::spawn(move || {
-    //     tx.send(42).unwrap();
-    // });
+    let c = Arc::new(Mutex::new(0));
+    let mut threads = vec![];
 
-    // println!("received {}", rx.recv().unwrap());
-
-    let (tx, rx) = mpsc::channel();
-    for i in 0..NUM_THREADS {
-        start_thread(i, tx.clone());
+    for i in 0..10 {
+        let c = Arc::clone(&c);
+        let t = thread::spawn(move || {
+            let mut num = c.lock().unwrap();
+            *num += 1;
+        });
+        threads.push(t);
     }
 
-    for j in rx.iter().take(NUM_THREADS) {
-        println!("received {}", j);
+    for th in threads {
+        th.join().unwrap();
     }
+
+    println!("Result {}", *c.lock().unwrap());
 }

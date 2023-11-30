ecc7ef825ba9de11efa7b8c3fdaa47bb489c9fc5
trong.dang <trong.dang@vieon.vn>
2023-11-30 14:10:34 +0700

    feat: trait impl Debug

--- activities/src/bin/demo.rs
+++ activities/src/bin/demo.rs
@@ -1681,24 +1681,101 @@
 //     }
 // }
 
-use std::ops::Add;
+// use std::ops::Add;
+
+// #[derive(Debug)]
+// struct Point<T> {
+//     x: T,
+//     y: T,
+// }
+
+// impl<T> Add for Point<T> where T: Add<Output = T> {
+//     type Output = Self;
+//     fn add(self, other: Self) -> Self {
+//         Self { x: self.x + other.x, y: self.y + other.y }
+//     }
+// }
+
+// fn main() {
+//     let coord = Point { x: 5.0, y: 5.0 };
+//     let coord2 = Point { x: 1.0, y: 2.0 };
+//     let coord3 = coord.add(coord2);
+//     println!("{:?}", coord3);
+// }
 
 #[derive(Debug)]
-struct Point<T> {
-    x: T,
-    y: T,
+struct Car {
+    mpg: i8,
+    color: String,
+    top_speed: i16,
 }
 
-impl<T> Add for Point<T> where T: Add<Output = T> {
-    type Output = Self;
-    fn add(self, other: Self) -> Self {
-        Self { x: self.x + other.x, y: self.y + other.y }
+#[derive(Debug)]
+struct Motorcycle {
+    mpg: i8,
+    color: String,
+    top_speed: i16,
+}
+
+pub trait Properties {
+    fn set_mpg(&mut self, mpg: i8);
+    fn set_color(&mut self, color: String);
+    fn set_top_speed(&mut self, top_speed: i16);
+}
+
+impl Properties for Car {
+    fn set_mpg(&mut self, mpg: i8) {
+        self.mpg = mpg;
+    }
+
+    fn set_color(&mut self, color: String) {
+        self.color = color;
+    }
+
+    fn set_top_speed(&mut self, top_speed: i16) {
+        self.top_speed = top_speed;
     }
 }
 
+impl Properties for Motorcycle {
+    fn set_mpg(&mut self, mpg: i8) {
+        self.mpg = mpg;
+    }
+
+    fn set_color(&mut self, color: String) {
+        self.color = color;
+    }
+
+    fn set_top_speed(&mut self, top_speed: i16) {
+        self.top_speed = top_speed;
+    }
+}
+
+fn print<T: std::fmt::Debug>(value: T) {
+    println!("{:?}", value);
+}
+
 fn main() {
-    let coord = Point { x: 5.0, y: 5.0 };
-    let coord2 = Point { x: 1.0, y: 2.0 };
-    let coord3 = coord.add(coord2);
-    println!("{:?}", coord3);
+    let mut car = Car {
+        mpg: 0,
+        color: String::from("Red"),
+        top_speed: 0,
+    };
+
+    let mut motorcycle = Motorcycle {
+        mpg: 0,
+        color: String::from("Green"),
+        top_speed: 0,
+    };
+
+    car.set_mpg(15);
+    car.set_color(String::from("blue"));
+    car.set_top_speed(250);
+
+    motorcycle.set_mpg(100);
+    motorcycle.set_color(String::from("red"));
+    motorcycle.set_top_speed(2500);
+
+    print(car);
+    print(motorcycle);
 }

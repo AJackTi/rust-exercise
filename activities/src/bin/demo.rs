49bba2960931ecc7a8db71cbda3831a09138825f
trong.dang <dtrong97vn@gmail.com>
2023-12-03 21:48:55 +0700

    feat: Error handling

--- activities/src/bin/demo.rs
+++ activities/src/bin/demo.rs
@@ -1723,79 +1723,90 @@
 //     println!("{:?}", coord3);
 // }
 
-#[derive(Debug)]
-struct Car {
-    mpg: i8,
-    color: String,
-    top_speed: i16,
-}
+// #[derive(Debug)]
+// struct Car {
+//     mpg: i8,
+//     color: String,
+//     top_speed: i16,
+// }
 
-#[derive(Debug)]
-struct Motorcycle {
-    mpg: i8,
-    color: String,
-    top_speed: i16,
-}
+// #[derive(Debug)]
+// struct Motorcycle {
+//     mpg: i8,
+//     color: String,
+//     top_speed: i16,
+// }
 
-pub trait Properties {
-    fn set_mpg(&mut self, mpg: i8);
-    fn set_color(&mut self, color: String);
-    fn set_top_speed(&mut self, top_speed: i16);
-}
+// pub trait Properties {
+//     fn set_mpg(&mut self, mpg: i8);
+//     fn set_color(&mut self, color: String);
+//     fn set_top_speed(&mut self, top_speed: i16);
+// }
 
-impl Properties for Car {
-    fn set_mpg(&mut self, mpg: i8) {
-        self.mpg = mpg;
-    }
+// impl Properties for Car {
+//     fn set_mpg(&mut self, mpg: i8) {
+//         self.mpg = mpg;
+//     }
 
-    fn set_color(&mut self, color: String) {
-        self.color = color;
-    }
+//     fn set_color(&mut self, color: String) {
+//         self.color = color;
+//     }
 
-    fn set_top_speed(&mut self, top_speed: i16) {
-        self.top_speed = top_speed;
-    }
-}
+//     fn set_top_speed(&mut self, top_speed: i16) {
+//         self.top_speed = top_speed;
+//     }
+// }
 
-impl Properties for Motorcycle {
-    fn set_mpg(&mut self, mpg: i8) {
-        self.mpg = mpg;
-    }
+// impl Properties for Motorcycle {
+//     fn set_mpg(&mut self, mpg: i8) {
+//         self.mpg = mpg;
+//     }
 
-    fn set_color(&mut self, color: String) {
-        self.color = color;
-    }
+//     fn set_color(&mut self, color: String) {
+//         self.color = color;
+//     }
 
-    fn set_top_speed(&mut self, top_speed: i16) {
-        self.top_speed = top_speed;
-    }
-}
+//     fn set_top_speed(&mut self, top_speed: i16) {
+//         self.top_speed = top_speed;
+//     }
+// }
 
-fn print<T: std::fmt::Debug>(value: T) {
-    println!("{:?}", value);
-}
+// fn print<T: std::fmt::Debug>(value: T) {
+//     println!("{:?}", value);
+// }
+
+// fn main() {
+//     let mut car = Car {
+//         mpg: 0,
+//         color: String::from("Red"),
+//         top_speed: 0,
+//     };
+
+//     let mut motorcycle = Motorcycle {
+//         mpg: 0,
+//         color: String::from("Green"),
+//         top_speed: 0,
+//     };
+
+//     car.set_mpg(15);
+//     car.set_color(String::from("blue"));
+//     car.set_top_speed(250);
+
+//     motorcycle.set_mpg(100);
+//     motorcycle.set_color(String::from("red"));
+//     motorcycle.set_top_speed(2500);
+
+//     print(car);
+//     print(motorcycle);
+// }
+
+use std::fs::File;
 
 fn main() {
-    let mut car = Car {
-        mpg: 0,
-        color: String::from("Red"),
-        top_speed: 0,
-    };
+    // panic!("This will cause the program to abruptly end");
 
-    let mut motorcycle = Motorcycle {
-        mpg: 0,
-        color: String::from("Green"),
-        top_speed: 0,
-    };
+    // let f = File::open("doesnotexist.txt").expect("No such thing");
+    let f = File::open("doesnotexist.txt").unwrap();
 
-    car.set_mpg(15);
-    car.set_color(String::from("blue"));
-    car.set_top_speed(250);
-
-    motorcycle.set_mpg(100);
-    motorcycle.set_color(String::from("red"));
-    motorcycle.set_top_speed(2500);
-
-    print(car);
-    print(motorcycle);
+    println!("The program is working here");
 }

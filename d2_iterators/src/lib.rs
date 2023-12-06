dbda0fdd2f1a01931235eaa1ca0aba3f63a87b1f
trong.dang <trong.dang@vieon.vn>
2023-12-06 12:36:30 +0700

    feat: iterator generic type

---
+++ d2_iterators/src/lib.rs
@@ -0,0 +1,46 @@
+mod gen_range;
+use std::ops::AddAssign;
+
+pub trait Rangeable: AddAssign + PartialOrd + Copy {}
+
+impl<T: AddAssign + PartialOrd + Copy> Rangeable for T {}
+
+pub struct GenRangeIterator<T: Rangeable> {
+    curr: T,
+    stop: T,
+    step: T,
+}
+
+impl<T: Rangeable> GenRangeIterator<T> {
+    pub fn new(start: T, stop: T, step: T) -> Self {
+        GenRangeIterator { curr: start, stop, step }
+    }
+}
+
+impl<T: Rangeable> Iterator for GenRangeIterator<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.curr >= self.stop {
+            return None;
+        }
+        let res = self.curr;
+        self.curr += self.step;
+        Some(res)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn gen_test() {
+        let mut m = 0;
+        let it = GenRangeIterator::new(5, 12, 3);
+        for s in it {
+            m += s;
+        }
+
+        assert_eq!(m, 5 + 8 + 11);
+    }
+}

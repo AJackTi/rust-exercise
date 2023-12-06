b4aea5dfbe3f34862fc2b471ce365d608d27a227
trong.dang <trong.dang@vieon.vn>
2023-12-06 12:56:34 +0700

    feat: discovering the itertools crate

--- d2_iterators/src/combi.rs
+++ d2_iterators/src/combi.rs
@@ -26,4 +26,27 @@ mod test_skip {
         let v: i32 = (0..10).skip_half().sum();
         assert_eq!(v, 1 + 3 + 5 + 7 + 9);
     }
+
+    #[test]
+    fn test_step_by() {
+        let v: i32 = (0..10).step_by(3).sum();
+        assert_eq!(v, 3 + 6 + 9);
+    }
+
+    use itertools::Itertools;
+    #[test]
+    fn test_interleave() {
+        let v: Vec<i32> = (0..4).interleave((11..15).rev()).collect();
+        assert_eq!(v, vec![0, 14, 1, 13, 2, 12, 3, 11]);
+    }
+
+    #[test]
+    fn test_intersperse() {
+        let s = "hello world etc";
+        let v: Vec<&str> = s.split(" ").intersperse(",").collect();
+        assert_eq!(v, vec!["hello", ",", "world", ",", "etc"]);
+
+        let s2 = s.split(" ").join(",");
+        assert_eq!(s2, "hello,world,etc");
+    }
 }

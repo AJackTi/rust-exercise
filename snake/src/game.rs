41e64a43d36817a88a45434a5f5517d530174b0f
trong.dang <dtrong97vn@gmail.com>
2023-12-15 16:48:07 +0700

    feat: draw the board

---
+++ snake/src/game.rs
@@ -0,0 +1,32 @@
+use piston_window::types::Color;
+use piston_window::*;
+
+use rand::{ thread_rng, Rng };
+use crate::draw::{ draw_block, draw_rectangle };
+
+const BORDER_COLOR: Color = [0.0, 0.0, 0.0, 1.0];
+
+pub struct Game {
+    width: i32,
+    height: i32,
+}
+
+impl Game {
+    pub fn new(width: i32, height: i32) -> Game {
+        Game {
+            width,
+            height,
+        }
+    }
+
+    pub fn draw(&self, con: &Context, g: &mut G2d) {
+        draw_rectangle(BORDER_COLOR, 0, 0, self.width, 1, con, g);
+        draw_rectangle(BORDER_COLOR, 0, self.height - 1, self.width, 1, con, g);
+        draw_rectangle(BORDER_COLOR, 0, 0, 1, self.height, con, g);
+        draw_rectangle(BORDER_COLOR, self.width - 1, 0, 1, self.height, con, g)
+    }
+
+    pub fn update(&mut self, delta_time: f64) {
+        
+    }
+}
--- snake/src/main.rs
+++ snake/src/main.rs
@@ -1,3 +1,30 @@
+use draw::to_coord;
+use game::Game;
+use piston_window::types::Color;
+use piston_window::*;
+
+extern crate piston_window;
+extern crate rand;
+
+mod draw;
+mod game;
+
+const BLACK_COLOR: Color = [0.5, 0.5, 0.5, 1.0];
+
 fn main() {
-    println!("Hello, world!");
+    let (width, height) = (30, 30);
+    let mut window: PistonWindow = WindowSettings::new("snake", [to_coord(width), to_coord(height)])
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    let mut game = Game::new(width, height);
+    while let Some(event) = window.next() {
+        window.draw_2d(&event, |c, g, _| {
+            clear(BLACK_COLOR, g);
+            game.draw(&c, g);
+        });
+
+        event.update(|args| { game.update(args.dt) });
+    }
 }

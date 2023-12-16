763d2bb25da886184169845e94e76125556faad8
trong.dang <dtrong97vn@gmail.com>
2023-12-16 22:47:29 +0700

    feat: try from, try into

--- activities/src/bin/a37.rs
+++ activities/src/bin/a37.rs
@@ -22,8 +22,48 @@
 // * Utilize the `thiserror` crate for your error type
 // * Run `cargo test --bin a37` to test your implementation
 
+use std::convert::TryFrom;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+enum RgbError {
+    #[error("hex code must begin with a hash (#)")]
+    MissingHash,
+    #[error("failed to parse hex digits: {0}")] ParseError(#[from] std::num::ParseIntError),
+    #[error("invalid hex color length (must be 6)")]
+    LengthError,
+}
+
 #[derive(Debug, Eq, PartialEq)]
 struct Rgb(u8, u8, u8);
+
+impl TryFrom<&str> for Rgb {
+    type Error = RgbError;
+    fn try_from(hex: &str) -> Result<Self, Self::Error> {
+        if !hex.starts_with('#') {
+            return Err(RgbError::MissingHash);
+        }
+        if hex.len() != 7 {
+            return Err(RgbError::LengthError);
+        }
+
+        let (r, g, b) = (
+            u8::from_str_radix(&hex[1..=2], 16)?,
+            u8::from_str_radix(&hex[3..=4], 16)?,
+            u8::from_str_radix(&hex[5..=6], 16)?,
+        );
+
+        Ok(Self(r, g, b))
+    }
+}
+
+/*
+impl From<std::num::ParseIntError> for RgbError {
+    fn from(error: std::num::ParseIntError) -> Self {
+        RgbError::ParseError(error)
+    }
+}
+*/
 
 fn main() {
     // Use `cargo test --bin a37` to test your implementation
@@ -38,11 +78,7 @@ mod test {
     fn converts_valid_hex_color() {
         let expected = Rgb(0, 204, 102);
         let actual = Rgb::try_from("#00cc66");
-        assert_eq!(
-            actual.is_ok(),
-            true,
-            "valid hex code should be converted to Rgb"
-        );
+        assert_eq!(actual.is_ok(), true, "valid hex code should be converted to Rgb");
         assert_eq!(actual.unwrap(), expected, "wrong Rgb value");
     }
 

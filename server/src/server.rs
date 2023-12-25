d6a47f42dce90810897527d72a1c937f79393d3b
trong.dang <dtrong97vn@gmail.com>
2023-12-25 18:31:39 +0700

    feat: logging incoming request

--- server/src/server.rs
+++ server/src/server.rs
@@ -21,7 +21,12 @@ impl Server {
             match listener.accept() {
                 Ok((mut stream, _)) => {
                     let mut buffer = [0; 1024];
-                    stream.read(&mut buffer).unwrap();
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                        }
+                        Err(e) => println!("Failed to read from connection: {}", e),
+                    }
                 }
                 Err(e) => {
                     println!("Failed to establish connection: {}", e);

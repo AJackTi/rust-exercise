da40a4fdf031a25d045e3785505dc928636cf1af
trong.dang <dtrong97vn@gmail.com>
2023-12-25 18:50:57 +0700

    feat: trait and type conversation

--- server/src/http/request.rs
+++ server/src/http/request.rs
@@ -1,5 +1,17 @@
+use super::method::Method;
+use std::convert::TryFrom;
+
+#[derive(Debug)]
 pub struct Request {
     path: String,
     query_string: Option<String>,
     method: super::method::Method,
+}
+
+impl TryFrom<&[u8]> for Request {
+    type Error = String;
+
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        unimplemented!()
+    }
 }
--- server/src/server.rs
+++ server/src/server.rs
@@ -1,5 +1,8 @@
+use crate::http::Request;
 use std::net::TcpListener;
 use std::io::Read;
+// use std::convert::TryFrom;
+// use std::convert::TryInto;
 
 pub struct Server {
     addr: String,
@@ -24,6 +27,14 @@ impl Server {
                     match stream.read(&mut buffer) {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+
+                            // Request::try_from(&buffer as &[u8]); // ok
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {
+                                    println!("Request: {:?}", request);
+                                }
+                                Err(e) => println!("Failed to parse request: {}", e),
+                            }
                         }
                         Err(e) => println!("Failed to read from connection: {}", e),
                     }

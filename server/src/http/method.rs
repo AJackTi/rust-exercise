0538fd5da8427102d1f1d41bf9e430f75c2a5ead
trong.dang <dtrong97vn@gmail.com>
2023-12-25 18:22:54 +0700

    feat: module

---
+++ server/src/http/method.rs
@@ -0,0 +1,12 @@
+pub enum Method {
+    GET,
+    POST,
+    PUT,
+    PATCH,
+    DELETE,
+    OPTIONS,
+    HEAD,
+    CONNECT,
+    TRACE,
+    UNKNOWN,
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -3,6 +3,9 @@ use server::Server;
 mod server;
 mod http;
 
+use http::Request;
+use http::Method;
+
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();

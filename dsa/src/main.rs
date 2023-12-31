e8346923d69d3ddcd9ae7587139cba368b8a365b
trong.dang <dtrong97vn@gmail.com>
2023-12-31 17:12:11 +0700

    feat: merge sort

--- dsa/src/main.rs
+++ dsa/src/main.rs
@@ -88,6 +88,35 @@ fn bubble_sort(array: &mut Vec<i32>) {
     array.to_vec();
 }
 
+fn merge_sort(arr: &mut [i32]) {
+    if arr.len() > 1 {
+        let mid = arr.len() / 2;
+        merge_sort(&mut arr[..mid]); // left half
+        merge_sort(&mut arr[mid..]); // right half
+        merge(arr, mid);
+    }
+
+    arr.to_vec();
+}
+
+fn merge(arr: &mut [i32], mid: usize) {
+    let left = arr[..mid].to_vec(); // left half to middle
+    let right = arr[mid..].to_vec(); // middle to right half
+
+    let mut l = 0;
+    let mut r = 0;
+
+    for val in arr {
+        if r == right.len() || (l < left.len() && left[l] < right[r]) {
+            *val = left[l];
+            l += 1;
+        } else {
+            *val = right[r];
+            r += 1;
+        }
+    }
+}
+
 fn main() {
     // println!("{:?}", fact(5));
 
@@ -104,10 +133,14 @@ fn main() {
     // let mut vec = vec![1, 2, 3, 4, 5];
     // triangle(&mut vec, 5);
 
-    let mut vec = vec![4, 3, 1, 2];
-    println!("Before sorting: {:?}", vec);
+    // let mut vec = vec![4, 3, 1, 2];
+    // println!("Before sorting: {:?}", vec);
     // selection_sort(&mut vec);
     // println!("After sorting: {:?}", vec);
-    bubble_sort(&mut vec);
-    println!("After sorting: {:?}", vec);
+    // bubble_sort(&mut vec);
+    // println!("After sorting: {:?}", vec);
+
+    let mut vec = vec![4, 7, 3, 5, 1, 2];
+    merge_sort(&mut vec);
+    println!("{:?}", vec);
 }

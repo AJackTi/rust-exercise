98f3ad2caccac0fb98a3da446e3754d3b9096051
trong.dang <dtrong97vn@gmail.com>
2023-12-31 14:11:50 +0700

    feat: Tower of Hanoi

--- dsa/src/main.rs
+++ dsa/src/main.rs
@@ -32,10 +32,25 @@ fn palindrom(array: &Vec<i32>, start: usize, end: usize) -> bool {
     return palindrom(array, start + 1, end - 1);
 }
 
+// Tower of Hanoi
+fn toh(n: i32) -> i32 {
+    if n == 0 {
+        return 0;
+    }
+
+    return toh(n - 1) + 1 + toh(n - 1);
+}
+
 fn main() {
-    println!("{:?}", fact(5));
+    // println!("{:?}", fact(5));
 
-    println!("{}", fib(15));
+    // println!("{}", fib(15));
 
-    println!("{:?}", palindrom(&vec![1, 2, 3, 2, 1], 0, 4));
+    // println!("{:?}", palindrom(&vec![1, 2, 3, 2, 1], 0, 4));
+
+    println!("{}", toh(0));
+    println!("{}", toh(1));
+    println!("{}", toh(2));
+    println!("{}", toh(3));
+    println!("{}", toh(4));
 }

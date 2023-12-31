a54fe89a7fd2e57bba677e476d564afcfcd1dbee
trong.dang <dtrong97vn@gmail.com>
2023-12-31 15:00:15 +0700

    feat: sum triangle of array

--- dsa/src/main.rs
+++ dsa/src/main.rs
@@ -41,6 +41,23 @@ fn toh(n: i32) -> i32 {
     return toh(n - 1) + 1 + toh(n - 1);
 }
 
+fn triangle(arr: &mut Vec<i8>, size: usize) {
+    if size < 1 {
+        return;
+    }
+
+    let mut tmp: Vec<i8> = Vec::new();
+
+    for i in 0..size - 1 {
+        let x = arr[i] + arr[i + 1];
+        tmp.push(x);
+    }
+
+    triangle(&mut tmp, size - 1);
+
+    println!("{:?}", arr);
+}
+
 fn main() {
     // println!("{:?}", fact(5));
 
@@ -48,9 +65,12 @@ fn main() {
 
     // println!("{:?}", palindrom(&vec![1, 2, 3, 2, 1], 0, 4));
 
-    println!("{}", toh(0));
-    println!("{}", toh(1));
-    println!("{}", toh(2));
-    println!("{}", toh(3));
-    println!("{}", toh(4));
+    // println!("{}", toh(0));
+    // println!("{}", toh(1));
+    // println!("{}", toh(2));
+    // println!("{}", toh(3));
+    // println!("{}", toh(4));
+
+    let mut vec = vec![1, 2, 3, 4, 5];
+    triangle(&mut vec, 5);
 }

1c88cb7bd948932bfdb86eeb843e2b63bcf76a9f
trong.dang <dtrong97vn@gmail.com>
2023-12-31 13:02:33 +0700

    feat: fibonacci

--- dsa/src/main.rs
+++ dsa/src/main.rs
@@ -5,6 +5,23 @@ fn fact(num: i32) -> i32 {
     return 1;
 }
 
+fn fib(num: i32) -> i32 {
+    if num == 0 {
+        return 0;
+    }
+
+    if num == 1 {
+        return 1;
+    }
+
+    let n1 = fib(num - 1);
+    let n2 = fib(num - 2);
+
+    return n1 + n2;
+}
+
 fn main() {
     println!("{:?}", fact(5));
+
+    println!("{}", fib(15));
 }

44085c223e8d4ec2b8797482eafbfb3c9ee00e51
trong.dang <dtrong97vn@gmail.com>
2023-12-31 12:56:11 +0700

    feat: factorial implement

--- dsa/src/main.rs
+++ dsa/src/main.rs
@@ -1,3 +1,10 @@
+fn fact(num: i32) -> i32 {
+    if num > 1 {
+        return num * fact(num - 1);
+    }
+    return 1;
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", fact(5));
 }
